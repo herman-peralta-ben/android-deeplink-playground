e3f46d2d3df4d815fe9d756387a6cf950252ff58
Herman Peralta <herman.peralta.ben@gmail.com>
2024-10-03 17:01:37 -0600

    [ADD] Button to test deeplink

--- app/src/main/java/dev/herman/androiddeeplinkplayground/MainActivity.kt
+++ app/src/main/java/dev/herman/androiddeeplinkplayground/MainActivity.kt
@@ -3,58 +3,41 @@ package dev.herman.androiddeeplinkplayground
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dev.herman.androiddeeplinkplayground.ui.theme.AndroidDeeplinkPlaygroundTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val intentAction: String? = intent?.action
-        val intentData: Uri? = intent?.data
-        Log.d("herman test action", intentAction ?: "null")
-        Log.d("herman test data", intentData?.toString() ?: "null")
-
         enableEdgeToEdge()
         setContent {
             AndroidDeeplinkPlaygroundTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Column(
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+                        Button(onClick = {
+                            val intent = Intent(
+                                Intent.ACTION_VIEW,
+                                Uri.parse("testing://www.testing.com/abc?name=herman&hobbies=coding,drums,games&age=36")
+                            )
+                            startActivity(intent)
+                        }) {
+                            Text(text = "Launch testing:// intent")
+                        }
+                    }
                 }
             }
         }
-    }
-
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidDeeplinkPlaygroundTheme {
-        Greeting("Android")
     }
 }
